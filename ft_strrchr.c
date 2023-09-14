1d745f142e3472cfc7d27a45a3982a3e2380b761
JayuSC2 <134278095+JayuSC2@users.noreply.github.com>
2023-09-14 20:04:05 +0200

    Add files via upload

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,48 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:16:42 by juitz             #+#    #+#             */
-/*   Updated: 2023/09/11 16:33:00 by juitz            ###   ########.fr       */
+/*   Updated: 2023/09/14 20:03:28 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-#include <strin.h>
+#include <string.h>
+#include "libft.h"
 
-int	ft_strlen	(char *str)
+char    *ft_strrchr(const char *s, int c)
 {
-	int	i;
+    int	i;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	i = ft_strlen(s);
+	while (i > 0)
+	{
+		if (s[i] == (char) c)
+			return ((char *)s + i);
+		i--;
+	}
+	return (0);
 }
-char    *strrchr(const char *s, int c)
+
+int	main(void)
 {
-    while (*str != '\0')   
+	 const char s[] = "Servus wie gehts";
+    int c = 'l';
+
+    char *result = ft_strrchr(s, c);
+    if (result != NULL)
+    {
+        printf("%s", result);
+    }
+    else
+    {
+        printf("Character not found");
+    }
+	return 0;
 }
+
+
+
+
+
+
